e46c9128d96ef841b8069a8230798ede1979a092
willross42 <will.ross.wr@gmail.com>
2021-03-26 13:39:29 +0000

    Added by_content_type. by_file_name and by_content_type  tests working
    basic by_content_type method added to  detect.py
    Test for by_file_name and by_content_type appear to be working.
    Test for by_sniff working for csv and xml but delimiter not detected for tsv.  Method issue or example file issue?

--- src/tests/test_detect_file.py
+++ src/tests/test_detect_file.py
@@ -1,65 +1,86 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from detect import file_ext_test, sniff_contents, detect_filetype
-from tests.mocks.response import ResponseMock
-from tests.utils import open_test_file
+from src.detect import by_file_name, by_content_type, by_sniff_contents, detect_filetype
+from src.tests.mocks.response import ResponseMock
+from src.tests.utils import open_test_file
 
 
 class DetectFileTypeTest(TestCase):
     def setUp(self):
         self.url_csv = "https://www.indiandcold.com/media/photo_export.csv"
         self.csv_file = 'csv_example.csv'
+        self.url_xml = "https://factionskis.com/pages/photoslurp-product-feed"
         self.xml_file = 'xml_example.xml'
+        self.url_tsv = "http://commondatastorage.googleapis.com/newfeedspec/example_feed_txt.zip"
         self.tsv_file = 'tsv_example.tsv'
 
     def test_by_filename(self):
-        file_type = file_ext_test(self.url_csv)
+        file_type = by_file_name(self.url_csv)
         self.assertEqual(file_type, "csv")
 
-        file_type = file_ext_test(self.url_xml)
+        file_type = by_file_name(self.xml_file)
         self.assertEqual(file_type, "xml")
 
-        file_type = file_ext_test(self.url_tsv)
+        file_type = by_file_name(self.tsv_file)
         self.assertEqual(file_type, "tsv")
 
-    def test_by_sniff(self):
-        with open_test_file(self.csv_file) as file:
-            self.assertEqual(sniff_contents(ResponseMock(fd=file)), "csv")
-
-        with open_test_file(self.tsv_file) as file:
-            self.assertEqual(sniff_contents(ResponseMock(fd=file)), "tsv")
-
-        with open_test_file(self.xml_file) as file:
-            self.assertEqual(sniff_contents(ResponseMock(fd=file)), "xml")
 
     def test_by_content_type_header(self):
         headers = {
             'Content-Type': 'text/csv'
         }
         with open_test_file(self.csv_file) as file:
-            self.assertEqual(detect_by_content_type(
+            self.assertEqual(by_content_type(
                 ResponseMock(fd=file, headers=headers)), "csv")
 
         headers = {
             'Content-Type': 'application/xml'
         }
         with open_test_file(self.xml_file) as file:
-            self.assertEqual(detect_by_content_type(
+            self.assertEqual(by_content_type(
                 ResponseMock(fd=file, headers=headers)), "xml")
 
         headers = {
             'Content-Type': 'text/tab-separated-values'
         }
         with open_test_file(self.tsv_file) as file:
-            self.assertEqual(detect_by_content_type(
+            self.assertEqual(by_content_type(
                 ResponseMock(fd=file, headers=headers)), "tsv")
 
-    def test_all_flow(self):
-        headers = {
-            'Content-Type': 'text/csv'
-        }
-        with patch('requests.get') as mock:
-            with open_test_file(self.csv_file) as file:
-                mock.return_value = ResponseMock(fd=file, headers=headers)
-                self.assertEqual(detect_filetype(self.url_csv), "csv")
+
+    def test_by_sniff_contents(self):
+        with open_test_file(self.csv_file) as file:
+            self.assertEqual(by_sniff_contents(ResponseMock(fd=file)), "csv")
+
+        with open_test_file(self.xml_file) as file:
+            self.assertEqual(by_sniff_contents(ResponseMock(fd=file)), "xml")
+
+        with open_test_file(self.tsv_file) as file:
+            self.assertEqual(by_sniff_contents(ResponseMock(fd=file)), "tsv")
+
+    #
+    # def test_all_flow(self):
+    #     headers = {
+    #         'Content-Type': 'text/csv'
+    #     }
+    #     with patch('requests.get') as mock:
+    #         with open_test_file(self.csv_file) as file:
+    #             mock.return_value = ResponseMock(fd=file, headers=headers)
+    #             self.assertEqual(detect_filetype(self.url_csv), "csv")
+
+    #     headers = {
+    #         'Content-Type': 'application/xml'
+    #     }
+    #     with patch('requests.get') as mock:
+    #         with open_test_file(self.xml_file) as file:
+    #             mock.return_value = ResponseMock(fd=file, headers=headers)
+    #             self.assertEqual(detect_filetype(self.url_xml), "xml")
+
+    #     headers = {
+    #         'Content-Type': 'text/tab-separated-values'
+    #     }
+    #     with patch('requests.get') as mock:
+    #         with open_test_file(self.tsv_file) as file:
+    #             mock.return_value = ResponseMock(fd=file, headers=headers)
+    #             self.assertEqual(detect_filetype(self.url_tsv), "tsv")
